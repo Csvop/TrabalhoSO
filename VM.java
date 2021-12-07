5f381a9c7c523e2041d9db1b3efb6910eac1b80b
Robson Felipe Bittencourt <Robson_Bittencourt@Dellteam.com>
2021-12-07 16:13:15 -0300

    Ajuste no dump available frames

--- VM.java
+++ VM.java
@@ -4,54 +4,52 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.Semaphore;
 
 public class VM {
-    //Hardwares
-    public int tamMem;  //ok 
-    public Word[] mem; //ok
-    public CPU cpu; //ok
+    // Hardwares
+    public int tamMem; // ok
+    public Word[] mem; // ok
+    public CPU cpu; // ok
 
-    //Software - Gerenciadores
-    public MemoryManager mm; //ok
-    public ProcessManager pm; //ok
-    public Scheduler scheduler; //ok
-    public LinkedList<PCB> readyQueue; //ok
-    public LinkedList<PCB> blockedQueue; //ok
-    public ConcurrentLinkedQueue<PCB> orderQueue; //ok
-    public Routines routine; //ok
+    // Software - Gerenciadores
+    public MemoryManager mm; // ok
+    public ProcessManager pm; // ok
+    public Scheduler scheduler; // ok
+    public LinkedList<PCB> readyQueue; // ok
+    public LinkedList<PCB> blockedQueue; // ok
+    public ConcurrentLinkedQueue<PCB> orderQueue; // ok
+    public Routines routine; // ok
     public Console console; // (THREAD)
 
-    //Semaforos
+    // Semaforos
     public Semaphore semCPU = new Semaphore(0);
     public Semaphore semESC = new Semaphore(0);
 
-    public VM(Semaphore semAllAPP){
-		//Hardware
-        tamMem = 512;
+    public VM(Semaphore semAllAPP) {
+        // Hardware
+        tamMem = 64;
         mem = blankMemory(tamMem);
 
         readyQueue = new LinkedList<PCB>();
         blockedQueue = new LinkedList<PCB>();
         orderQueue = new ConcurrentLinkedQueue<PCB>();
-        
+
         cpu = new CPU(this.mem, this.semCPU, this.semESC);
 
         mm = new MemoryManager(this.mem);
-        
-        pm = new ProcessManager(mm, readyQueue, semESC);
-        
-        scheduler = new Scheduler(readyQueue, semESC, semCPU, cpu);
-        
-        console = new Console(cpu, mem, semAllAPP);
-        
-        routine = new Routines(pm, scheduler, semESC, blockedQueue, console);
 
+        pm = new ProcessManager(mm, readyQueue, semESC);
+
+        scheduler = new Scheduler(readyQueue, semESC, semCPU, cpu);
+
+        console = new Console(cpu, mem, semAllAPP);
+
+        routine = new Routines(pm, scheduler, semESC, blockedQueue, console);
 
         // Configura o processo inicial
         cpu.configure(routine, mm);
-        
+
         // Init threads
         this.init();
     }
-
 
     public void load(Word[] program) {
         pm.createProcess(program);
@@ -84,14 +82,6 @@ public class VM {
             i++;
             SystemOut.print("[" + i + "] (" + frames[i] + ") --- ");
             i++;
-            SystemOut.print("[" + i + "] (" + frames[i] + ") --- ");
-            i++;
-            SystemOut.print("[" + i + "] (" + frames[i] + ") --- ");
-            i++;
-            SystemOut.print("[" + i + "] (" + frames[i] + ") --- ");
-            i++;
-            SystemOut.print("[" + i + "] (" + frames[i] + ") --- ");
-            i++;
             SystemOut.log("[" + i + "] (" + frames[i] + ")");
         }
         SystemOut.print("\n");
@@ -100,10 +90,10 @@ public class VM {
     public void init() {
         cpu.setName("CPU");
         cpu.start();
-        
+
         scheduler.setName("Scheduler");
         scheduler.start();
-        
+
         console.setName("Console");
         console.start();
     }
